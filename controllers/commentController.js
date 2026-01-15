0cdb38c7bf5ec3b4b5492530136003983ef9c657
ShimiManashirov <122288727+ShimiManashirov@users.noreply.github.com>
2026-01-15 23:45:35 +0200

    Add update and delete comment (#7)
    * add_update_and_delete_post
    
    * add_update_and_delete_post

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -23,7 +23,32 @@ const createComment = async (req, res) => {
     }
 };
 
+const updateComment = async (req, res) => { 
+    const commentId = req.params.commentId;
+    const updatedData = req.body;
+    try {
+        const updatedComment = await commentmodel.findByIdAndUpdate(commentId, updatedData, { new: true });
+        res.json(updatedComment);
+    }      catch (error) { 
+        console.error(error);
+        res.status(500).json({ message: 'Error updating comment', error: error.message });
+    }
+};
+
+const deleteComment = async (req, res) => { 
+    const commentId = req.params.commentId;
+    try {
+        await commentmodel.findByIdAndDelete(commentId);
+        res.status(204).end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error deleting comment', error: error.message });
+    }
+}
+
 module.exports = {
     getCommentsForPost,
-    createComment
+    createComment,
+    updateComment,
+    deleteComment
 };
